b6a5a54399e8a68d0473c0dc9ce73b7d6776765a
Hanuman Chu <chuhanuman@gmail.com>
2018-02-22 00:05:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var recipes = {}
-function updateObjectWithKeyAndValue(object, val, value) {
-  return Object.assign({}, object, {val:value})
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {"key":value})
 }
